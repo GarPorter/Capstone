05482cf288316161fa2ceb89e4611f592cf079e3
GarPorter <123344694+GarPorter@users.noreply.github.com>
2023-01-23 12:50:22 -0800

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[3]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
+
+st.title("Testing out Push to GitHub and Streamlit app")
+
+
 # In[ ]:
 
 
