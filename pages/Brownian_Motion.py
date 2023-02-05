c3f7d170385fee8455846c819c49858fcb5b360f
GarPorter <123344694+GarPorter@users.noreply.github.com>
2023-02-05 15:37:21 -0800

    Fix2

--- pages/Brownian_Motion.py
+++ pages/Brownian_Motion.py
@@ -7,7 +7,14 @@ st.write('''Brownian motion aims to quantify the random motion of particles
             suspended in a medium. Utilizing Gaussian distribution we can track 
             the motion of a particle through a medium''')
 
-Modb = st.selectbox("How long would you like to walk?", 100, 200, 300, key = "<four>")
+Modb = st.selectbox("How long would you like to walk?", ("Short", "Medium", "Long"))
+
+if Modb == "Short":
+    t=10
+elif Modb == "Medium":
+    t=50
+elif Modb == "Long":
+    t=100
 
 x=np.random.normal(0,1,Modb)
 y=np.random.normal(0,1,Modb)
