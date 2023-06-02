cbdcd0b0391b275136ecd7c5b106366e82ae7769
boris <borispopov10@gmail.com>
2023-06-01 18:15:54 -0700

    update documentation

--- Backend/SvgToPoints.py
+++ Backend/SvgToPoints.py
@@ -1,6 +1,15 @@
 import svgpathtools
 
 def svg_to_points(svg_file, n_points=2):
+    """Converts an SVG file to a list of points array
+
+    Args:
+        svg_file (str): path to SVG file
+        n_points (int, optional): number of points to sample along each cubic Bezier curve. Defaults to 2.
+
+    Returns:
+        2D list: list of lists of point coordinates as tuples that represent the pattern path
+    """
     paths, attributes = svgpathtools.svg2paths(svg_file)
     point_arrays = []
     for path in paths:
